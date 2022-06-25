97da7be174faec121bd2ec9aac78d36bae2f402c
maxusify <maxusify@gmail.com>
2022-06-26 00:17:18 +0200

    Dist folder removal

--- server/dist/prisma/generated/type-graphql/resolvers/relations/UserFavoriteSongs/args/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserFavoriteSongsSongsArgs = void 0;
-var UserFavoriteSongsSongsArgs_1 = require("./UserFavoriteSongsSongsArgs");
-Object.defineProperty(exports, "UserFavoriteSongsSongsArgs", { enumerable: true, get: function () { return UserFavoriteSongsSongsArgs_1.UserFavoriteSongsSongsArgs; } });
-//# sourceMappingURL=index.js.map
--- server/dist/prisma/generated/type-graphql/resolvers/relations/UserProfile/args/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserProfilePostsArgs = exports.UserProfileCommentsArgs = void 0;
-var UserProfileCommentsArgs_1 = require("./UserProfileCommentsArgs");
-Object.defineProperty(exports, "UserProfileCommentsArgs", { enumerable: true, get: function () { return UserProfileCommentsArgs_1.UserProfileCommentsArgs; } });
-var UserProfilePostsArgs_1 = require("./UserProfilePostsArgs");
-Object.defineProperty(exports, "UserProfilePostsArgs", { enumerable: true, get: function () { return UserProfilePostsArgs_1.UserProfilePostsArgs; } });
-//# sourceMappingURL=index.js.map
